33e24fc607a4883c7b277eea07a20923752f29fb
Louis Blonde <louis.blonde24@gmail.com>
2026-03-28 11:18:23 +0100

    update searching bar

--- app/components/SearchBar/SearchBar.tsx
+++ app/components/SearchBar/SearchBar.tsx
@@ -20,28 +20,29 @@ export default function searchBar() {
         };
     };
 
-        const [resultSearch, setResultSearch] = useState<any>([]);
+        const [resultSearch, setResultSearch] = useState([]);
         const [search, setSearch] = useState("");
 
         useEffect(() => {
-            const delay = setTimeout(() => {
-                searchUser();
-            }, 100)
-
-            return () => clearTimeout(delay)
-        }, [search])
+            searchUser();
+        }, [search]);
 
     return (
         <form className="relative">
-            <input onChange={(e) => setSearch(`${e.target.value}`)} type="text" placeholder="Search a user..." value={search} />
-            <div className="absolute top-full left-0 w-full mt-2 bg-red border border-gray-200 rounded-lg shadow-lg z-50 max-h-60 overflow-y-auto">
-                {resultSearch.map((e: any, i: number) => {
-                    return (
-                        <div key={i}>{e.username}</div>
-                    );
-                })}
+            <input className="ml-4" onChange={(e) => setSearch(`${e.target.value}`)} type="text" placeholder="Search a user..." value={search} />
+            <button onClick={(e) => e.preventDefault()} className="ml-5 mr-4 hover:cursor-pointer" type="submit" >Search</button>
+            <div>
+                {resultSearch.length === 0 ? (<div></div>) : (
+                    <div className="absolute left-0 w-full mt-2 border-2 p-2 border-gray-200 rounded-lg shadow-lg z-50 max-h-60 overflow-y-auto">
+                        {resultSearch.map((e: any, i: number) => {
+                            return (
+                                <a className="flex rounded pl-2 pt-1 pb-1 duration-300 hover:text-black hover:bg-gray-200 hover:cursor-pointer" href={`/usr/${e.username_lower}`} key={i}>{e.username}</a>
+                            );
+                        })}
+                    </div>
+                )}
             </div>
-            <button onClick={(e) => e.preventDefault()} className="hover:cursor-pointer" type="submit" >Search</button>
+            
         </form>
     )
 }
